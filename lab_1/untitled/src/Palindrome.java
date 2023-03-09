41ef4c257145403bd65901e3be29538f86f5d098
Overlord <vip.nazari@inbox.ru>
2023-03-09 22:08:33 +0300

    make polindrom program

--- lab_1/untitled/src/Palindrome.java
+++ lab_1/untitled/src/Palindrome.java
@@ -1,2 +1,27 @@
+
 public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            if (isPalindrome(s)) {
+                System.out.println(s+" is Palindrom");
+            }else{
+                System.out.println(s+"is not Palindrom");
+            }
+        }
+    }
+    public static String reverseString(String s){
+        String revs = "";//Создаем доп переменную для реверсирования
+        for (int i=0; i<s.length();i++){
+            revs += s.charAt(s.length() - i-1);
+        }
+        return revs;
+    }
+    public static boolean isPalindrome(String s){
+        if (s.equalsIgnoreCase(reverseString(s))) {
+            return true;
+        }
+        return false;
+    }
+
 }
