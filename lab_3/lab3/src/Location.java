d745cde454c7afd0332bd7b7e3920844ad86a15d
Overlord <vip.nazari@inbox.ru>
2023-04-09 14:46:38 +0300

    add lab_3 programm and otch

---
+++ lab_3/lab3/src/Location.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Location))
+            return false;
+        Location loc = (Location) obj;
+        return xCoord == loc.xCoord && yCoord == loc.yCoord;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xCoord,yCoord);
+    }
+}
